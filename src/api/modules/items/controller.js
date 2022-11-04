9467083a3e55904251e4967fa41952e03e46a088
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-11-03 22:51:05 -0300

    fix: added catch to promise all

--- src/api/modules/items/controller.js
+++ src/api/modules/items/controller.js
@@ -1,50 +1,53 @@
-const fetch = require('node-fetch');
-const builderUrl = require('./helpers/builderUrl');
-const searchResultsItem = require('./helpers/mapper/searchResultsItem');
-const searchResultsItems = require('./helpers/mapper/searchResultsItems');
+const fetch = require("node-fetch");
+const builderUrl = require("./helpers/builderUrl");
+const searchResultsItem = require("./helpers/mapper/searchResultsItem");
+const searchResultsItems = require("./helpers/mapper/searchResultsItems");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const BASE_URL = process.env.BASE_URL;
 
-const getItems = async( req, res ) => {
-
+const getItems = async (req, res) => {
   try {
     const { sort, filters, product } = req.body;
-    const url = builderUrl( product, sort, filters );
-    console.log( url )
-    const resp_items = await fetch( url );
+    const url = builderUrl(product, sort, filters);
+    console.log(url);
+    const resp_items = await fetch(url);
 
-    const filtered_data = await searchResultsItems( resp_items );
-    res.status(200).send( filtered_data );
-    
+    const filtered_data = await searchResultsItems(resp_items);
+    res.status(200).send(filtered_data);
   } catch (error) {
-    console.log( error );  
+    console.log(error);
   }
-}
+};
 
-const getItem = async( req, res ) => {
-  
+const getItem = async (req, res) => {
   try {
     const { item_id } = req.params;
-    const resp_item = await fetch(`${ BASE_URL }/items/${ item_id }`);
-    console.log(`${ BASE_URL }/items/${ item_id }`)
+    const resp_item = await fetch(`${BASE_URL}/items/${item_id}`);
+    console.log(`${BASE_URL}/items/${item_id}`);
     const data_item = await resp_item.json();
-    
-    const [ seller_data, questions_data, description_data, reviews_data ] = await Promise.all([
-      fetch(`${ BASE_URL }/users/${ data_item.seller_id }`),
-      fetch(`${ BASE_URL }/questions/search?item_id=${ item_id }`),
-      fetch(`${ BASE_URL }/items/${ item_id }/description`),
-      fetch(`${ BASE_URL }/reviews/item/${ item_id }`),
-    ]);
 
-    const filtered_data = await searchResultsItem( seller_data, questions_data, description_data, reviews_data, data_item );
+    const [seller_data, questions_data, description_data, reviews_data] =
+      await Promise.all([
+        fetch(`${BASE_URL}/users/${data_item.seller_id}`),
+        fetch(`${BASE_URL}/questions/search?item_id=${item_id}`),
+        fetch(`${BASE_URL}/items/${item_id}/description`),
+        fetch(`${BASE_URL}/reviews/item/${item_id}`),
+      ]).catch((e) => console.log(e));
 
-    res.status(200).send( filtered_data ); 
+    const filtered_data = await searchResultsItem(
+      seller_data,
+      questions_data,
+      description_data,
+      reviews_data,
+      data_item
+    );
 
+    res.status(200).send(filtered_data);
   } catch (error) {
-    console.log( error ); 
+    console.log(error);
   }
-}
+};
 
 module.exports = { getItem, getItems };
